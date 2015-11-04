de4c6ca1186accaf5627b72bda164cf0183329ac
Josue Camara <picharras@gmail.com>
2015-11-04 09:31:56 -0600

    Add devicecollect

--- conektasdk/src/main/java/com/conekta/conektasdk/Connection.java
+++ conektasdk/src/main/java/com/conekta/conektasdk/Connection.java
@@ -1,48 +1,80 @@
 package com.conekta.conektasdk;
 
-import android.os.AsyncTask;
-import org.json.JSONObject;
-import org.apache.http.client.HttpClient;
-import org.apache.http.HttpResponse;
-import org.apache.http.util.EntityUtils;
-import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.HttpClient;
+import org.apache.http.util.EntityUtils;
+import org.apache.http.NameValuePair;
+import org.apache.http.HttpResponse;
+import android.os.AsyncTask;
+import android.util.Base64;
+import java.util.ArrayList;
+import android.util.Log;
+import java.util.List;
 
 /**
  * Created by picharras on 27/10/15.
  */
-public class Connection extends  AsyncTask<Void, Integer, Boolean> {
-    JSONObject jsonResult;
+public class Connection {
+    private List<NameValuePair> nameValuePair;
+    private String endPoint;
 
-    @Override
-    protected Boolean doInBackground(Void... params) {
-        HttpClient http = new DefaultHttpClient();
-        HttpPost httpDomain = new HttpPost(Conekta.baseUril);
-        httpDomain.setHeader("Accept", "application/json");
-        httpDomain.setHeader("content-type", "application/json");
+    public interface Request {
+        public void onRequestReady(String data);
+    }
 
-        try {
-            HttpResponse response = http.execute(httpDomain);
-            String result = EntityUtils.toString(response.getEntity());
-            jsonResult = new JSONObject(result);
-        } catch (Exception ex) {
-            System.console();
+    private Request listener;
+
+    public Connection() {
+        this.listener = null;
+    }
+
+    // Assign the listener implementing events interface that will receive the events
+    public void onRequestListener (Request listener) {
+        this.listener = listener;
+    }
+
+    public void request(List<NameValuePair> nameValuePair, String endPoint) {
+        this.nameValuePair = nameValuePair;
+        this.endPoint = endPoint;
+        new Task().execute();
+    }
+
+    private class Task extends AsyncTask<Void, Void, String> {
+
+        protected String doInBackground(Void...params) {
+            String result;
+
+            HttpClient http = new DefaultHttpClient();
+            HttpPost httpRequest = new HttpPost(Conekta.getBaseUri() + endPoint);
+            String encoding = Base64.encodeToString( Conekta.getPublicKey().getBytes(), Base64.NO_WRAP);
+
+            try {
+                httpRequest.setHeader("Accept", "application/vnd.conekta-v" + Conekta.getApiVersion() + "+json");
+                httpRequest.setHeader("Accept-Language", Conekta.getLanguage());
+                httpRequest.setHeader("Conekta-Client-User-Agent", "{\"agent\": \"Conekta Android SDK\"}");
+                httpRequest.setHeader("Authorization", "Basic " + encoding);
+                httpRequest.setEntity(new UrlEncodedFormEntity(nameValuePair));
+
+                HttpResponse response = http.execute(httpRequest);
+                result = EntityUtils.toString(response.getEntity());
+            } catch (Exception ex) {
+                throw new RuntimeException(ex);
+            }
+            return result;
         }
-        return true;
-    }
 
-    @Override
-    protected void onProgressUpdate(Integer... values) {
+        @Override
+        protected void onPreExecute() {
 
-    }
+        }
 
-    @Override
-    protected void onPreExecute() {
-        //progress = ProgressDialog.show(Main.this, getString(R.string.looking),getString(R.string.looking_wait));
-    }
-
-    @Override
-    protected void onPostExecute(Boolean result) {
+        @Override
+        protected void onPostExecute(String result) {
+            listener.onRequestReady(result);
+        }
 
     }
 
--- conektasdk/src/main/java/com/conekta/conektasdk/Token.java
+++ conektasdk/src/main/java/com/conekta/conektasdk/Token.java
@@ -1,19 +1,65 @@
 package com.conekta.conektasdk;
 
+import android.app.Activity;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by picharras on 27/10/15.
  */
 public class Token {
-    private String endPoint = "/token";
+    private String endPoint = "/tokens";
+    private Activity activity;
 
-    public Token() {
-
+    public interface CreateToken {
+        public void onCreateTokenReady(JSONObject data);
     }
 
-    public String create(Card card) {
+    private CreateToken listener;
+
+    public Token() {
+        this.listener = null;
+    }
+
+    public void onCreateTokenListener (CreateToken listener) {
+        this.listener = listener;
+    }
+
+    public Token(Activity activity) {
+        this.activity = activity;
+    }
+
+    public void create(Card card) {
+        /* Remember add to index of nameValuePair if you add more rows */
+        List<NameValuePair> nameValuePair = new ArrayList<NameValuePair>(6);
+
+        nameValuePair.add(new BasicNameValuePair("card[number]", card.getNumber()));
+        nameValuePair.add(new BasicNameValuePair("card[name]", card.getName()));
+        nameValuePair.add(new BasicNameValuePair("card[cvc]", card.getCvc()));
+        nameValuePair.add(new BasicNameValuePair("card[exp_month]", card.getExpMonth()));
+        nameValuePair.add(new BasicNameValuePair("card[exp_year]", card.getExpYear()));
+        nameValuePair.add(new BasicNameValuePair("card[device_fingerprint]", Conekta.collectDevice(activity)));
+
         Connection connection = new Connection();
-        connection.execute();
-        String hola = "";
-        return hola;
+
+        connection.onRequestListener(new Connection.Request() {
+            @Override
+            public void onRequestReady(String data) {
+                JSONObject obj;
+                try {
+                     obj = new JSONObject(data);
+
+                } catch (Exception err) {
+                    obj = new JSONObject();
+                }
+                listener.onCreateTokenReady(obj);
+            }
+        });
+        connection.request(nameValuePair, this.endPoint);
     }
 }
