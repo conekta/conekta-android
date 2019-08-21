84fe2ed7f56644852788da801e93595284af0077
erickCTA <erick.dominguez@conekta.com>
2019-08-21 12:55:25 -0500

    update / refactored android SDK
    Why is this change neccesary?
    
    To keep the SDK updated for new implementations
    
    How does it address the issue?
    
    Refactoring all the SDK code
    
    What side effects does this change have?
    
    Can affect the functionality for old clients

--- app/src/main/java/io/conekta/helloconekta/Form.java
+++ app/src/main/java/io/conekta/helloconekta/Form.java
@@ -1,5 +1,6 @@
+//------------------------------------------------------------- Application form
 package io.conekta.helloconekta;
-
+//------------------------------------------------------------- Imports
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.content.Context;
@@ -11,88 +12,57 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import org.json.JSONObject;
 import android.util.Log;
+import org.json.JSONObject;
 
 import io.conekta.conektasdk.Card;
 import io.conekta.conektasdk.Conekta;
 import io.conekta.conektasdk.Token;
-
+//------------------------------------------------------------- Application sdk form class
+/**
+ * Application form class
+ */
 public class Form extends Activity {
-
-    private Button btnTokenize;
-    private TextView outputView;
-    private TextView uuidDevice;
-    private EditText numberText;
-    private EditText monthText;
-    private EditText yearText;
-    private EditText cvcText;
-    private EditText nameText;
+    //--------------------------------------------------------- Variables
+    private Button buttonTokenize;
     private Activity activity = this;
-
+    private TextView outputView, uuidDevice;
+    private EditText numberText, monthText, yearText, cvcText, nameText;
+    private String cardName, cardNumber, cardCvc, cardMonth, cardYear; //card data strings
+    private String tokenIdTag, errorTag, uuidDeviceTag; //tags strings
+    //--------------------------------------------------------- Constants
+    private static final String PUBLIC_KEY = "zbp4axNG4xVUMcDzTLNz";
+    private static final String API_VERSION = "0.3.0";
+    //--------------------------------------------------------- Methods
+    /**
+     * Is a method of Android System where initialize the activity.
+     * Initialize views on this function
+     * Initialize inputs listeners
+     * @param savedInstanceState is the default Bundle of Android
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_form);
 
-        btnTokenize = (Button) findViewById(R.id.btnTokenize);
-        outputView = (TextView) findViewById(R.id.outputView);
-        uuidDevice = (TextView) findViewById(R.id.uuidDevice);
-        numberText = (EditText) findViewById(R.id.numberText);
-        nameText = (EditText) findViewById(R.id.nameText);
-        monthText = (EditText) findViewById(R.id.monthText);
-        yearText = (EditText) findViewById(R.id.yearText);
-        cvcText = (EditText) findViewById(R.id.cvcText);
-
-        btnTokenize.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Boolean haveInternet = isOnline();
-                if (haveInternet) {
-
-                    Conekta.setPublicKey("zbp4axNG4xVUMcDzTLNz");
-                    Conekta.setApiVersion("0.3.0");
-                    Conekta.collectDevice(activity);
-                    Card card = new Card(nameText.getText().toString(), numberText.getText().toString(), cvcText.getText().toString(), monthText.getText().toString(), yearText.getText().toString());
-                    Token token = new Token(activity);
-
-                    //Listen when token is returned
-                    token.onCreateTokenListener(new Token.CreateToken() {
-
-                        @Override
-                        public void onCreateTokenReady(JSONObject data) {
-
-                            try {
-                                Log.d("The token::::", data.getString("id"));
-                                outputView.setText("Token id: " + data.getString("id"));
-                            } catch (Exception err) {
-                                outputView.setText("Error: " + err.toString());
-                            }
-                            uuidDevice.setText("Uuid device: " + Conekta.deviceFingerPrint(activity));
-                        }
-                    });
-
-                    //Request for create token
-                    token.create(card);
-                } else {
-                    outputView.setText("You don't have internet");
-                }
-            }
-        });
+        initializeViewForm();
+        setListeners();
     }
 
-    public boolean isOnline () {
-        ConnectivityManager cm =
-                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo netInfo = cm.getActiveNetworkInfo();
-        if (netInfo != null && netInfo.isConnected()) {
-            return true;
-        }
-        return false;
+    /**
+     * System override method, launched when the user enter to the activity form.
+     */
+    @Override
+    protected void onStart() {
+        super.onStart();
+        initializeTags();
     }
 
-
+    /**
+     * System override method to create a option menu bar
+     * @param menu menu type
+     * @return always value true
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -100,18 +70,129 @@ public class Form extends Activity {
         return true;
     }
 
+    /**
+     * Method to add items to the menu bar
+     * @param item type item
+     * @return true value if an item is chosen otherwise false
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        int itemId = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (itemId == R.id.action_settings) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Method to initialize tags text
+     */
+    private void initializeTags(){
+        tokenIdTag = getResources().getString(R.string.theTokenIdLabel); // The token id:
+        errorTag = getResources().getString(R.string.errorLabel); // Error:
+        uuidDeviceTag = getResources().getString(R.string.uuidDeviceLabel); // Uuid device:
+    }
+
+    /**
+     * Initialize all inputs from view
+     */
+    private void initializeViewForm(){
+        buttonTokenize = findViewById(R.id.btnTokenize);
+        outputView = findViewById(R.id.outputView);
+        uuidDevice = findViewById(R.id.uuidDevice);
+        numberText = findViewById(R.id.numberText);
+        nameText = findViewById(R.id.nameText);
+        monthText = findViewById(R.id.monthText);
+        yearText = findViewById(R.id.yearText);
+        cvcText = findViewById(R.id.cvcText);
+    }
+
+    /**
+     * Method to set inputs listeners
+     */
+    private void setListeners(){
+        buttonTokenize.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onPressTokenizeButton();
+            }
+        });
+    }
+
+    /**
+     * Method triggered when press tokenize button to get a token from the card data received
+     */
+    private void onPressTokenizeButton(){
+        if (hasInternetConnection()) {
+            Conekta.setPublicKey(PUBLIC_KEY);
+            Conekta.setApiVersion(API_VERSION);
+            Conekta.collectDevice(activity);
+
+            getCardData();
+
+            Card card = new Card(cardName, cardNumber, cardCvc, cardMonth, cardYear);
+            Token token = new Token(activity);
+
+            //Listen when token is returned
+            token.onCreateTokenListener(new Token.CreateToken() {
+                @Override
+                public void onCreateTokenReady(JSONObject data) {
+                    showTokenResult(data);
+                }
+            });
+
+            //Request for create token
+            token.create(card);
+        } else {
+            outputView.setText(getResources().getString(R.string.needInternetConnection));
+        }
+    }
+
+    /**
+     * Method to get card data
+     */
+    private void getCardData(){
+        cardName = nameText.getText().toString();
+        cardNumber = numberText.getText().toString();
+        cardCvc = cvcText.getText().toString();
+        cardMonth = monthText.getText().toString();
+        cardYear = yearText.getText().toString();
+    }
+
+    /**
+     * Method to check device internet connection
+     * @return true value in case of success internet connection otherwise false
+     */
+    private boolean hasInternetConnection () {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+
+        return netInfo != null && netInfo.isConnected();
+    }
+
+    /**
+     * Method to show on device the token result
+     * @param data token data
+     */
+    private void showTokenResult(JSONObject data){
+        try {
+            String tokenId = data.getString("id");
+            String tokenMessage = tokenIdTag +" "+ tokenId;
+
+            outputView.setText(tokenMessage);
+            Log.d(tokenIdTag, tokenId);
+        } catch (Exception error) {
+            String errorMessage = errorTag +" "+ error.toString();
+            outputView.setText(errorMessage);
+        }
+
+        String uuidMessage = uuidDeviceTag +" "+ Conekta.deviceFingerPrint(activity);
+        uuidDevice.setText(uuidMessage);
     }
 }
